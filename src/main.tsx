4782bfa6a17bb392da48ba6e26152545caa2b7a5
Paweł <186290161+skipthedocs@users.noreply.github.com>
2025-03-22 20:39:39 +0100

    chore: update formatting in package.json and index.css; add biome-ignore comment in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./App.tsx";
 
 import "./index.css";
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
